a6dcddb2a340ff4080a56d1bd544dc8cd7c09ba9
yosepjeon <enekelx1@naver.com>
2021-04-20 16:46:45 +0900

    second service apigate를 custom filter 적용을 위한 수정

--- second-service/src/main/java/com/yosep/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/yosep/secondservice/SecondServiceController.java
@@ -21,4 +21,9 @@ public class SecondServiceController {
 
         return header;
     }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there. This is a message from Second Service.";
+    }
 }
