7c40eb82fbe067ab42cac586c639fd5b74b38ea0
yosepjeon <enekelx1@naver.com>
2021-04-20 15:27:21 +0900

    gateway service filter 적용

--- second-service/src/main/java/com/yosep/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/yosep/secondservice/SecondServiceController.java
@@ -1,14 +1,24 @@
 package com.yosep.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/second-service")
+@Slf4j
 public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request")String header) {
+        log.info(header);
+
+        return header;
     }
 }
